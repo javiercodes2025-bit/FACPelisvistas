ae190c9043e57bd917715b31a0cceda5f2d5c49c
Mario3d <mario3d-email@ejemplo.com>
2026-06-24 12:28:57 -0300

    ANTES DEL BUSCADOR FAC

--- FACTURACION.cpp
+++ FACTURACION.cpp
@@ -3,6 +3,14 @@
 #include "Clientes.h"
 #include "Empleados.h"
 #include "Stock.h"
+#include <msclr/marshal_cppstd.h>
+#include <jdbc/cppconn/driver.h>
+#include <jdbc/cppconn/exception.h>
+#include <jdbc/cppconn/statement.h>
+#include <jdbc/mysql_connection.h>
+#include <jdbc/mysql_driver.h>
+#include <jdbc/cppconn/prepared_statement.h>
+#include "peliVAR.h"
 
 void FACPelisvistas::FACTURACION::proveedoresBTN_Click(System::Object^ sender, System::EventArgs^ e) {
 	this->Hide();
@@ -23,5 +31,38 @@ void FACPelisvistas::FACTURACION::stockBTN_Click(System::Object^ sender, System:
 	this->Hide();
 	FACPelisVistas::STOCK^ stk = gcnew FACPelisVistas::STOCK;
 	stk->Show();
+}
+void FACPelisvistas::FACTURACION::buscadorClienteBTN_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (String::IsNullOrWhiteSpace(buscadorClienteBTX->Text)) return;
+	std::string doc = msclr::interop::marshal_as<std::string>(buscadorClienteBTX->Text);
+	try {
+		if (!BDDins || BDDins->isClosed()) {
+			conexionCls temp("", "", "", "", "", "", "", "", "", "", "");
+			temp.conectVoid();
+		}
+		sql::PreparedStatement* ps = BDDins->prepareStatement(
+			"SELECT p.nombre, p.apellido "
+			"FROM persona p "
+			"JOIN personaXdocumento pdoc ON p.id_persona = pdoc.fk_empleado "
+			"JOIN documento doc ON pdoc.fk_persona = doc.id_documento "
+			"WHERE doc.numero_documento = ? AND doc.habilitado = 1 "
+			"AND p.habilitado = 1 AND p.rol = 'CLIENTE'"
+		);
+		ps->setString(1, sql::SQLString(doc));
+		sql::ResultSet* rs = ps->executeQuery();
+		if (rs->next()) {
+			nombreClienteBTX->Text = gcnew String(rs->getString("nombre").c_str()) + " " +
+				gcnew String(rs->getString("apellido").c_str());
+		}
+		else {
+			MessageBox::Show("No existe");
+			nombreClienteBTX->Text = L"";
+		}
+		delete rs;
+		delete ps;
+	}
+	catch (sql::SQLException& ex) {
+		MessageBox::Show(gcnew String(ex.what()));
+	}
 }
 
--- FACTURACION.h
+++ FACTURACION.h
@@ -38,6 +38,14 @@ namespace FACPelisvistas {
 	private: System::Windows::Forms::Button^ empleadosBTN;
 	private: System::Windows::Forms::Button^ proveedoresBTN;
 	private: System::Windows::Forms::Button^ stockBTN;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ buscadorClienteBTX;
+	private: System::Windows::Forms::Button^ buscadorClienteBTN;
+
+	private: System::Windows::Forms::Label^ NOM;
+	private: System::Windows::Forms::TextBox^ nombreClienteBTX;
+
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
 
 
 
@@ -62,6 +70,13 @@ namespace FACPelisvistas {
 			this->empleadosBTN = (gcnew System::Windows::Forms::Button());
 			this->proveedoresBTN = (gcnew System::Windows::Forms::Button());
 			this->stockBTN = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->buscadorClienteBTX = (gcnew System::Windows::Forms::TextBox());
+			this->buscadorClienteBTN = (gcnew System::Windows::Forms::Button());
+			this->NOM = (gcnew System::Windows::Forms::Label());
+			this->nombreClienteBTX = (gcnew System::Windows::Forms::TextBox());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// clienteBTN
@@ -104,11 +119,67 @@ namespace FACPelisvistas {
 			this->stockBTN->UseVisualStyleBackColor = true;
 			this->stockBTN->Click += gcnew System::EventHandler(this, &FACTURACION::stockBTN_Click);
 			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(26, 73);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(87, 13);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"C.DOCUMENTO";
+			// 
+			// buscadorClienteBTX
+			// 
+			this->buscadorClienteBTX->Location = System::Drawing::Point(29, 101);
+			this->buscadorClienteBTX->Name = L"buscadorClienteBTX";
+			this->buscadorClienteBTX->Size = System::Drawing::Size(181, 20);
+			this->buscadorClienteBTX->TabIndex = 5;
+			// 
+			// buscadorClienteBTN
+			// 
+			this->buscadorClienteBTN->Location = System::Drawing::Point(359, 99);
+			this->buscadorClienteBTN->Name = L"buscadorClienteBTN";
+			this->buscadorClienteBTN->Size = System::Drawing::Size(99, 23);
+			this->buscadorClienteBTN->TabIndex = 6;
+			this->buscadorClienteBTN->Text = L"BuscadorCliente";
+			this->buscadorClienteBTN->UseVisualStyleBackColor = true;
+			this->buscadorClienteBTN->Click += gcnew System::EventHandler(this, &FACTURACION::buscadorClienteBTN_Click);
+			// 
+			// NOM
+			// 
+			this->NOM->AutoSize = true;
+			this->NOM->Location = System::Drawing::Point(224, 85);
+			this->NOM->Name = L"NOM";
+			this->NOM->Size = System::Drawing::Size(54, 13);
+			this->NOM->TabIndex = 7;
+			this->NOM->Text = L"NOMBRE";
+			// 
+			// nombreClienteBTX
+			// 
+			this->nombreClienteBTX->Location = System::Drawing::Point(227, 101);
+			this->nombreClienteBTX->Name = L"nombreClienteBTX";
+			this->nombreClienteBTX->Size = System::Drawing::Size(126, 20);
+			this->nombreClienteBTX->TabIndex = 8;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(26, 598);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(832, 144);
+			this->dataGridView1->TabIndex = 9;
+			// 
 			// FACTURACION
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(870, 692);
+			this->ClientSize = System::Drawing::Size(870, 754);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->nombreClienteBTX);
+			this->Controls->Add(this->NOM);
+			this->Controls->Add(this->buscadorClienteBTN);
+			this->Controls->Add(this->buscadorClienteBTX);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->stockBTN);
 			this->Controls->Add(this->proveedoresBTN);
 			this->Controls->Add(this->empleadosBTN);
@@ -116,7 +187,9 @@ namespace FACPelisvistas {
 			this->Name = L"FACTURACION";
 			this->Text = L"FACTURACION";
 			this->Load += gcnew System::EventHandler(this, &FACTURACION::FACTURACION_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -128,5 +201,6 @@ private: System::Void FACTURACION_Load(System::Object^ sender, System::EventArgs
 private: System::Void empleadosBTN_Click(System::Object^ sender, System::EventArgs^ e);
 private: System::Void clienteBTN_Click(System::Object^ sender, System::EventArgs^ e);
 private: System::Void stockBTN_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void buscadorClienteBTN_Click(System::Object^ sender, System::EventArgs^ e);
 };
 }
